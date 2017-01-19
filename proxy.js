c37c57667c683d94435ddf8e8d6616da9dfd853f
Richard Kelsey <richard.kelsey@ericsson.com>
2017-01-19 16:15:05 +0000

    Simplify

--- proxy.js
+++ proxy.js
@@ -1,62 +1,32 @@
 var IN_HOST = '10.20.9.1';
 var OUT_HOST = '10.0.0.1';
+var OUT_PORT = '54321';
 
-
-var INCOMING_MULTICAST = process.argv[2];
-var INCOMING_PORT = process.argv[3];
-var DESTINATION_MULTICAST = process.argv[4];
-var DESTINATION_PORT = process.argv[5];
-var DESTINATION_MULTICAST2 = process.argv[6];
-var DESTINATION_PORT2 = process.argv[7];
+var INCOMING_MULTICAST = "224.1.4.100";
+var INCOMING_PORT = "2000";
+var DESTINATION_MULTICAST = "239.100.1.2";
+var DESTINATION_PORT = "5000";
 
 var dgram = require('dgram');
+var incoming = dgram.createSocket({type: 'udp4', reuseAddr: true});
+var outgoing = dgram.createSocket({type: 'udp4', reuseAddr: true});
 
-var incoming = [];
-incoming.push(
-    {
-        socket:            dgram.createSocket({type: 'udp4', reuseAddr: true}),
-        multicast_address: INCOMING_MULTICAST,
-        multicast_port:    INCOMING_PORT,
-        packetDropCount:   0
-    }
-);
-
-var outgoing = []
-outgoing.push(
-    {
-        socket:            dgram.createSocket({type: 'udp4', reuseAddr: true}),
-        multicast_address: DESTINATION_MULTICAST,
-        multicast_port:    DESTINATION_PORT,
-        packetDropCount:   0
-    }
-);
-
-
-outgoing.forEach(function(og){
-    og.socket.bind(OUT_HOST);
-    og.socket.on('listening', function() {
-        og.socket.setBroadcast(true);
-        og.socket.setMulticastTTL(128);
-    });
+outgoing.bind(OUT_PORT, OUT_HOST);
+outgoing.on('listening', function() {
+    var address = outgoing.address();
+    console.log('Broadcasting on ' + address.address + ":" + address.port);
+    outgoing.setBroadcast(true);
+    outgoing.setMulticastTTL(128);
 });
 
-incoming.forEach(function(ig) {
-    ig.socket.on('listening', function () {
-        var address = ig.socket.address();
-        console.log('UDP Client listening on ' + address.address + ":" + address.port);
-        ig.socket.setBroadcast(true)
-        ig.socket.setMulticastTTL(128);
-        ig.socket.addMembership(ig.multicast_address, HOST);
-    });
+incoming.on('listening', function () {
+    var address = incoming.address();
+    console.log('Listening on ' + address.address + ":" + address.port);
+    incoming.addMembership(INCOMING_MULTICAST, IN_HOST);
 });
 
-incoming[0].socket.on('message', function (incoming_packet, remote) {
-    outgoing.forEach(function(og) {
-        og.socket.send(incoming_packet,
-                       0,
-                       incoming_packet.length,
-                       og.multicast_port, og.multicast_address);
-    });
+incoming.on('message', function (incoming_packet, remote) {
+    outgoing.send(incoming_packet, 0, incoming_packet.length, DESTINATION_PORT, DESTINATION_MULTICAST);
 });
 
-incoming[0].socket.bind(incoming[0].multicast_port, IN_HOST);
+incoming.bind(INCOMING_PORT);
