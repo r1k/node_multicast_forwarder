b63ac1f3237a2a4ebdb3ebd8b7ca5a60e9c62e74
Richard Kelsey <richard.kelsey@ericsson.com>
2017-08-25 16:58:06 +0100

    Add control params

--- proxy.js
+++ proxy.js
@@ -1,17 +1,47 @@
-var IN_HOST = '10.20.9.1';
-var OUT_HOST = '10.0.0.1';
-var OUT_PORT = '54321';
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
 
-var INCOMING_MULTICAST = "224.1.4.100";
-var INCOMING_PORT = "2000";
-var DEST_MULTICAST = "239.100.1.2";
-var DEST_PORT = "5000";
+const optionDefinitions = [
+    { name: 'iniface', type: String, description: "The source interface address", typeLabel: "<addr>"},
+    { name: 'inmcast', type: String, description: "The source multicast address", typeLabel: "<addr>" },
+    { name: 'inport', type: String, description: "The source port number", typeLabel: "<port>" },
+    { name: 'outiface', type: String, description: "The output interface", typeLabel: "<addr>" },
+    { name: 'outmcast', type: String, description: "The output address", typeLabel: "<addr>" },
+    { name: 'outport', type: String, description: "The output port", typeLabel: "<port>" },
+    { name: 'help', alias: 'h', type: Boolean, description: "Display the help"}
+  ]
+
+const options = commandLineArgs(optionDefinitions, { partial: false });
+    
+function show_help(){
+    const usage = commandLineUsage([
+        {
+        header: 'Typical Example',
+        content: 'A simple example demonstrating typical usage.'
+        },
+        {
+        header: 'Options',
+        optionList: optionDefinitions
+        }
+    ])
+    console.log(usage)
+    process.exit();
+}
+
+if (options.help){
+    show_help();
+}
+
+if (!(options.iniface && options.inmcast && options.inport && options.outiface && options.outmcast && options.outport)) {
+    show_help();
+}
+
 
 var dgram = require('dgram');
 var incoming = dgram.createSocket({type: 'udp4', reuseAddr: true});
 var outgoing = dgram.createSocket({type: 'udp4', reuseAddr: true});
 
-outgoing.bind(OUT_PORT, OUT_HOST);
+outgoing.bind(options.outport, options.outiface);
 outgoing.on('listening', function() {
     console.log('Broadcasting on ' + this.address().address + ":" + this.address().port);
     this.setBroadcast(true);
@@ -20,9 +50,11 @@ outgoing.on('listening', function() {
 
 incoming.on('listening', function () {
     console.log('Listening on ' + this.address().address + ":" + this.address().port);
-    this.addMembership(INCOMING_MULTICAST, IN_HOST);
+    this.addMembership(options.inmcast, options.iniface);
 });
 
-incoming.on('message', (packet) => { outgoing.send(packet, 0, packet.length, DEST_PORT, DEST_MULTICAST); });
+incoming.on('message', (packet) => {
+    outgoing.send(packet, 0, packet.length, options.outport, options.outmcast);
+});
 
-incoming.bind(INCOMING_PORT);
+incoming.bind(options.inport);
